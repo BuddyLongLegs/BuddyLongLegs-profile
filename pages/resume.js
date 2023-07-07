71e93c1ce54d1144508ab949e15375090b6703a9
BuddyLongLegs <anurag10jain@gmail.com>
2023-07-07 16:40:25 +0530

    updating resume

--- pages/resume.js
+++ pages/resume.js
@@ -1,27 +1,23 @@
-import Head from 'next/head'
-import styles from "../styles/Resume.module.css"
+import Head from "next/head";
+import styles from "../styles/Resume.module.css";
 import styles1 from "../styles/Nav.module.css";
 import styles2 from "../styles/Home.module.css";
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 import handleAnimation from "../lib/aniUseEffectHandler";
 
-export default function Connect(props){
-    useEffect(()=>{
-      handleAnimation(props);
-    }, [props])
-    
-    return (
-      <div>
-        <Head>
-          <title>Resume · BuddyLongLegs</title>
-        </Head>
-        <div
-          className={[styles2.container, styles1.invisible, styles.cont].join(
-            " "
-          )}
-        >
-          <iframe src="/Resume-22-01-2023.pdf" className={styles.pdf}></iframe>
-        </div>
+export default function Connect(props) {
+  useEffect(() => {
+    handleAnimation(props);
+  }, [props]);
+
+  return (
+    <div>
+      <Head>
+        <title>Resume · BuddyLongLegs</title>
+      </Head>
+      <div className={[styles2.container, styles1.invisible, styles.cont].join(" ")}>
+        <iframe src="/Resume_7_Jul_2023.pdf" className={styles.pdf}></iframe>
       </div>
-    );
+    </div>
+  );
 }
